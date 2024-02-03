35980799007d22d9dde30d8501f130dd540349f6
david_perso <davidboulanger2003@gmail.com>
2024-02-03 16:30:29 -0500

    rencontre1 david - analyse prel

--- ACT4114_TP_pt1_rapport_Equipe13.R
+++ ACT4114_TP_pt1_rapport_Equipe13.R
@@ -36,16 +36,17 @@ data("pg15training")
 data.analyse <- pg15training
 
 ##
-## 2. Pré-traitement des variables
+## 2. Pré-traitement des variables / Correction d'erreurs
 ##
 str(data.analyse)
 summary(data.analyse)
 
 # Polices en double
 ids <- data.analyse[duplicated(data.analyse$PolNum), "PolNum"]
+
 data.analyse[data.analyse$PolNum %in% ids[1], ]
 data.analyse[data.analyse$PolNum %in% ids[2], ]
-# les 21 premières ?
+# les 21 premières ? voir rapport
 (data.analyse$PolNum %in% ids)[1:22] # oui, on doit les enlever
 data.analyse <- data.analyse[-(1:21), ]
 any(duplicated(data.analyse$PolNum))
@@ -57,8 +58,22 @@ nrow(data.analyse)
 colnames(data.analyse)
 
 # Numtppd (endogène)
+(freq <- table(data.analyse$Numtppd))
+# Wow 7 accidents !!!
+fmp <- freq / 100000
+fmp
+# Graphique de la fmp
+plot(fmp, main="Fonction de masse de probabilité empirique\npour le nombre d'accidents",
+     lwd=4, col="red", xlab="Nombre d'accidents", ylab="Proportion")
+mean(data.analyse$Numtppd)
+var(data.analyse$Numtppd)
+summary(data.analyse$Numtppd)
 
 # Exppdays (exposition)
+table(data.analyse$Exppdays)
+summary(data.analyse$Exppdays)
+hist(data.analyse$Exppdays / 365)
+# très grande proportion de police complète!
 
 # Variables exclues :
 # - Polnum
@@ -86,6 +101,7 @@ ggplot(data.analyse, aes(x=Numtppd))+
     geom_bar(aes(y = after_stat(prop), fill=Type), position="dodge")+
     scale_y_continuous(labels = scales::percent)
 data.analyse %>% group_by(Type) %>% summarise(Moyenne = mean(Numtppd))
+plot(data.analyse %>% group_by(Type) %>% summarise(Moyenne = mean(Numtppd)))
 
 # Category
 
@@ -108,10 +124,29 @@ ggplot(moy_par_age) + geom_point(aes(x=Age, y=Moyenne), col="darkblue", size=2.5
     geom_errorbar(aes(x=Age, ymin=low_ic, ymax=upp_ic), width=0.5) +
     labs(title = "Moyenne de réclamation pour la couverture étudiée\nen fonction de l'age avec un IC approximatif 80%",
          y="Moyenne des réclamtations")
-# graphique de l'ecart-type estimé en fct de l'age
-ggplot(moy_par_age) + geom_point(aes(x=Age, y=dev_std))
+# graphique du coef de variation en fct de l'age
+ggplot(moy_par_age) + geom_point(aes(x=Age, y=dev_std/Moyenne))
+# Très dure è interpréter adéquatement, mais intéressant
 
 # Group1
+table(data.analyse$Group1)
+
+# D'accord, bcp de niveaux faisons une analyse similaire à l'age
+moy_par_group1 <- data.analyse %>% group_by(Group1) %>% summarise(
+    Count=length(Numtppd), Moyenne=mean(Numtppd), dev_std=sd(Numtppd)
+)
+moy_par_group1
+
+# IC approximatif 80 % par group1
+moy_par_group1$low_ic <- moy_par_group1$Moyenne + qnorm(0.1)*moy_par_group1$dev_std/sqrt(moy_par_group1$Count)
+moy_par_group1$upp_ic <- moy_par_group1$Moyenne + qnorm(0.9)*moy_par_group1$dev_std/sqrt(moy_par_group1$Count)
+# graphique moyenne d'accidents par group1 avec IC 80 %
+ggplot(moy_par_group1) + geom_point(aes(x=Group1, y=Moyenne), col="darkblue", size=2.5) +
+    geom_errorbar(aes(x=Group1, ymin=low_ic, ymax=upp_ic), width=0.5) +
+    labs(title = "Moyenne de réclamation pour la couverture étudiée\nen fonction de 'Group1' avec un IC approximatif 80%",
+         y="Moyenne des réclamtations")
+# Très linéaire Wow!
+
 
 # Bonus
 
